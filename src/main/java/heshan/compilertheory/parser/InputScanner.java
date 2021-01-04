7fb4f6513c07f06bd9602ac237c531f06a33d312
Heshan <hpheshan@gmail.com>
2021-01-04 10:04:04 +0530

    Initial commit with code from private repository

---
+++ src/main/java/heshan/compilertheory/parser/InputScanner.java
@@ -0,0 +1,58 @@
+package heshan.compilertheory.parser;
+
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class InputScanner implements Iterator<String>{
+    Scanner scanner;
+    Pattern blockStart = Pattern.compile("\\{.*");
+    Pattern blockEnd = Pattern.compile(".+}.*");
+    Pattern blockEndPrefix = Pattern.compile(".+}");
+    Pattern lineStart = Pattern.compile("#.*");
+    Pattern emptyLine = Pattern.compile("\n?");
+    Pattern multipleWhiteSpaces = Pattern.compile("\\s+");
+
+    public InputScanner(Path input_file) throws FileNotFoundException {
+        scanner = new Scanner(input_file.toFile()).useDelimiter("\n");
+    }
+
+    private void skipComments(){
+        if(scanner.hasNext(blockStart)){
+            String token = scanner.next();
+            while(token.charAt(token.length()-1) != '}'){
+                token = scanner.next();
+            }
+            skipComments();
+        }
+        if (scanner.hasNext(lineStart) || scanner.hasNext(emptyLine)){
+            scanner.next();
+            skipComments();
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        skipComments();
+        return scanner.hasNext();
+    }
+
+    @Override
+    public String next() {
+        skipComments();
+        String token = scanner.next();
+        token = lineStart.matcher(token).replaceAll("");
+        if (blockStart.matcher(token).matches()){
+            token = blockStart.matcher(token).replaceAll("");
+            while(!scanner.hasNext(blockEnd)){
+                scanner.next();
+            }
+        }
+        token = blockEndPrefix.matcher(token).replaceAll("");
+        token = multipleWhiteSpaces.matcher(token).replaceAll(" ");
+        return token;
+    }
+}
+
---
+++ src/test/java/heshan/compilertheory/parser/InputScannerTest.java
@@ -0,0 +1,37 @@
+package heshan.compilertheory.parser;
+
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputScannerTest {
+    Pattern multipleWhiteSpaces = Pattern.compile("\\s{2,}");
+
+    @org.junit.jupiter.api.Test
+    void canReadAllInputs(){
+        Path assetsRoot = Paths.get("", "src", "test", "resources");
+        var inputFiles = Arrays.stream(Objects.requireNonNull(assetsRoot.toFile().listFiles())).filter(filename -> !filename.toString().contains(".tree"));
+        inputFiles.forEach(file -> {
+            try {
+                InputScanner scanner = new InputScanner(file.toPath());
+                while(scanner.hasNext()){
+                    String token = scanner.next();
+                    assertFalse(multipleWhiteSpaces.matcher(token).matches(),
+                            String.format("multiple whitespaces detected in token \"%s\"", token)); // no empty strings
+                    assertFalse(token.contains("#"),
+                            String.format("line comment detected in token \"%s\"", token)); // no line comments
+                    assertFalse(token.contains("{") || token.contains("}"),
+                            String.format("block comment detected in token \"%s\"", token)); // no block comments
+                }
+            } catch (FileNotFoundException e) {
+                assertNull(e);
+            }
+
+        });
+    }
+}
