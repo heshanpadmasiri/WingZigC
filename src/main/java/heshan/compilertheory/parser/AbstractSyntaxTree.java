a6c6647896be2eca79b8ec3421b7d62970dae964
Heshan <hpheshan@gmail.com>
2021-01-15 16:07:16 +0530

    Added writer function to write the ast to a file

--- src/main/java/heshan/compilertheory/parser/AbstractSyntaxTree.java
+++ src/main/java/heshan/compilertheory/parser/AbstractSyntaxTree.java
@@ -1,5 +1,8 @@
 package heshan.compilertheory.parser;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.Collections;
 
 public class AbstractSyntaxTree {
@@ -17,10 +20,27 @@ public class AbstractSyntaxTree {
     }
   }
 
+  private void writeNode(ASTNode node, int level, FileWriter fileWriter) throws IOException {
+    fileWriter.write(
+            String.format("%s%s\n", String.join("", Collections.nCopies(level, ". ")), node));
+    for (ASTNode child : node.getChildren() ) {
+      writeNode(child, level+1, fileWriter);
+    }
+  }
+
   public void printTree() {
     printNode(root, 0);
   }
 
+  public void toFile(Path outputFile) throws IOException {
+   if(outputFile.toFile().exists()){
+     System.out.println("Overwriting existing output file");
+     outputFile.toFile().delete();
+   }
+   FileWriter fileWriter = new FileWriter(outputFile.toString());
+   writeNode(root, 0, fileWriter);
+   fileWriter.close();
+  }
 
   public ASTNode getRoot() {
     return root;
--- src/test/java/heshan/compilertheory/parser/AbstractSyntaxTreeTest.java
+++ src/test/java/heshan/compilertheory/parser/AbstractSyntaxTreeTest.java
@@ -3,9 +3,14 @@ package heshan.compilertheory.parser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -13,34 +18,67 @@ import static org.mockito.Mockito.when;
 
 class AbstractSyntaxTreeTest {
 
-    SymbolTable symbolTable;
+  SymbolTable symbolTable;
 
-    @BeforeEach
-    void setUp() {
-        symbolTable = mock(SymbolTable.class);
-        when(symbolTable.getNextKey()).thenReturn(0);
+  @BeforeEach
+  void setUp() {
+    symbolTable = mock(SymbolTable.class);
+    when(symbolTable.getNextKey()).thenReturn(0);
+  }
+
+  void printTree(Path program_path) {
+    try {
+      InputScanner scanner = new InputScanner(program_path);
+      RecursiveDescentParser parser = new RecursiveDescentParser(symbolTable, scanner);
+      parser.parse();
+      AbstractSyntaxTree ast = parser.getAbstractSyntaxTree();
+      ast.printTree();
+    } catch (FileNotFoundException | FailedToMatchPatternException e) {
+      assertNull(e);
     }
+  }
 
-    void printTree(Path program_path, Path ast_path){
-        try {
-            InputScanner scanner = new InputScanner(program_path);
+  @Test
+  void writeToFile() {
+    Path assetsRoot = Paths.get("", "src", "test", "resources");
+    Stream<File> inputFiles =
+        Arrays.stream(Objects.requireNonNull(assetsRoot.toFile().listFiles()))
+            .filter(
+                filename ->
+                    !(filename.toString().contains(".tree")
+                        || filename.toString().contains(".test")));
+    inputFiles.forEach(
+        file -> {
+          try {
+            InputScanner scanner = new InputScanner(file.toPath());
             RecursiveDescentParser parser = new RecursiveDescentParser(symbolTable, scanner);
             parser.parse();
+            String ast_file_name = file.getName() + ".ast";
+            Path ast_file = assetsRoot.resolve(ast_file_name);
             AbstractSyntaxTree ast = parser.getAbstractSyntaxTree();
-            ast.printTree();
-        } catch (FileNotFoundException | FailedToMatchPatternException e) {
+            ast.toFile(ast_file);
+          } catch (FailedToMatchPatternException | IOException e) {
             assertNull(e);
-        }
-    }
+          }
+        });
+  }
 
-    @Test
-    void printTree() {
-        Path program = Paths.get("", "src", "test", "resources", "winzig_01");
-        Path ast = Paths.get("", "src", "test", "resources", "winzig_01.tree");
-        printTree(program, ast);
-    }
+  @Test
+  void printTree() {
+    Path assetsRoot = Paths.get("", "src", "test", "resources");
+    Stream<File> inputFiles =
+        Arrays.stream(Objects.requireNonNull(assetsRoot.toFile().listFiles()))
+            .filter(
+                filename ->
+                    !(filename.toString().contains(".tree")
+                        || filename.toString().contains(".test")));
+    inputFiles.forEach(
+        file -> {
+          System.out.println(file);
+          printTree(file.toPath());
+        });
+  }
 
-    @Test
-    void getRoot() {
-    }
+  @Test
+  void getRoot() {}
 }
